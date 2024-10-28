d2c245627a103b95f3ab851de526b510e859af1c
Documenter.jl <documenter@juliadocs.github.io>
2024-10-28 16:42:21 +0000

    build based on b7e3c32

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GadgetSearch","category":"page"},{"location":"#GadgetSearch","page":"Home","title":"GadgetSearch","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GadgetSearch.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GadgetSearch]","category":"page"}]
+}
