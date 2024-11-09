1d11922a4d3b363f3169f274b373a420aced3564
GiggleLiu <cacate0129@gmail.com>
2024-11-09 08:23:35 +0800

    fix path specification

--- test/utils.jl
+++ test/utils.jl
@@ -1,5 +1,5 @@
 @testset "GraphIO" begin
-    path = "../data/graphs/graph2.g6"
+    path = pkgdir(GadgetSearch, "data", "graphs", "graph2.g6")
     graph_dict = readGraphDictFile(path)
     @test readGraphFile(path, 1) == graph_dict["graph1"]
 end
@@ -7,37 +7,37 @@ end
 @testset "graphPlot" begin
     vertex_num = [3]
     for i in vertex_num
-        path = "../data/graphs/graph$i.g6"
+        path = pkgdir(GadgetSearch, "data", "graphs", "graph$i.g6")
         graph_dict = readGraphDictFile(path)
-        if !isdir("../data/graphplot/$(i)vertex/")
-            mkpath("../data/graphplot/$(i)vertex/")
+        if !isdir(pkgdir(GadgetSearch, "data", "graphplot", "$(i)vertex/"))
+            mkpath(pkgdir(GadgetSearch, "data", "graphplot", "$(i)vertex/"))
         end
-        plotGraphs(graph_dict, "../data/graphplot/$(i)vertex/")
+        plotGraphs(graph_dict, pkgdir(GadgetSearch, "data", "graphplot", "$(i)vertex/"))
     end
 end
 
 @testset "loadJSON & locate" begin
-    path = "../datasets/logic_gates/2in_2out.json"
+    path = pkgdir(GadgetSearch, "datasets", "logic_gates", "2in_2out.json")
     data = loadJSONFile(path)
     @test length(data) == 256
     degeneracy = ["0011", "0101", "1010", "1100"]
     g_info = findByDegeneracy(data, degeneracy)
-    plotColoredGraph(g_info, "../data/graphplot/")
-    rm("../data/graphplot/graph.png")
+    plotColoredGraph(g_info, pkgdir(GadgetSearch, "data", "graphplot"))
+    rm(pkgdir(GadgetSearch, "data", "graphplot", "graph.png"))
     _, _, ground_states = checkGraphMIS(g_info)
     @test Set(degeneracy) == Set(ground_states)
 
-    path = "../datasets/any_constraint/2bits_any_constraint.json"
+    path = pkgdir(GadgetSearch, "datasets", "any_constraint", "2bits_any_constraint.json")
     data = loadJSONFile(path)
     @test length(data) == 15
-    path = "../datasets/any_constraint/3bits_any_constraint.json"
+    path = pkgdir(GadgetSearch, "datasets", "any_constraint", "3bits_any_constraint.json")
     data = loadJSONFile(path)
     @test length(data) == 255
     
     degeneracy = ["001", "010", "100", "111"]
     g_info = findByDegeneracy(data, degeneracy)
-    plotColoredGraph(g_info, "../data/graphplot/")
-    rm("../data/graphplot/graph.png")
+    plotColoredGraph(g_info, pkgdir(GadgetSearch, "data", "graphplot"))
+    rm(pkgdir(GadgetSearch, "data", "graphplot", "graph.png"))
     _, _, ground_states = checkGraphMIS(g_info)
     @test Set(degeneracy) == Set(ground_states)
 end
